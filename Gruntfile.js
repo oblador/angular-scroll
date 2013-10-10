8920c0d1068fa3cb73a28538fc2c81ea5ca1378e
Joel Arvidsson <joel@oblador.se>
2013-10-10 19:43:07 +0200

    Fixed jshinting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
         src: 'Gruntfile.js'
       },
       lib: {
-        src: ['dist/**/*.js']
+        src: ['src/**/*.js']
       },
     },
     clean: {
