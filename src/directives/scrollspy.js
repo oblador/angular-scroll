4852d011b957804bd5612ce25c69f1f6e3717692
Joel Arvidsson <joel@oblador.se>
2014-01-16 12:54:19 +0100

    Refactored scroll spy handling.

--- src/directives/scrollspy.js
+++ src/directives/scrollspy.js
@@ -4,17 +4,37 @@ directive('duScrollspy', function($rootScope, scrollPosition) {
   var currentlyActive;
   var isObserving = false;
 
+  var Spy = function(targetElementOrId, $element, offset) {
+    if(angular.isElement(targetElementOrId)) {
+      this.target = targetElementOrId;
+    } else if(angular.isString(targetElementOrId)) {
+      this.targetId = targetElementOrId;
+    }
+    this.$element = $element;
+    this.offset = offset;
+  };
+
+  Spy.prototype.getTargetElement = function() {
+    if (!this.target && this.targetId) {
+      this.target = document.getElementById(this.targetId);
+    }
+    return this.target;
+  };
+
+  Spy.prototype.getTargetPosition = function() {
+    var target = this.getTargetElement();
+    if(target) {
+      return target.getBoundingClientRect();
+    }
+  };
+
   function gotScroll($event, scrollY) {
     var toBeActive;
     for(var spy, scroll, pos, i = 0; i < spies.length; i++) {
       spy = spies[i];
+      pos = spy.getTargetPosition();
+      if (!pos) continue;
 
-      if (!spy.target) {
-        spy.target = document.getElementById(spy.targetId);
-        if (!spy.target) continue;
-      }
-
-      pos = spy.target.getBoundingClientRect();
       if(pos.top + spy.offset < 20 && pos.top*-1 < pos.height) {
         if(!toBeActive || toBeActive.top < pos.top) {
           toBeActive = {
@@ -33,18 +53,22 @@ directive('duScrollspy', function($rootScope, scrollPosition) {
     currentlyActive = toBeActive;
   }
 
-  function addSpy(targetId, $element, offset) {
+  function addSpy(spy) {
     if(!isObserving) {
       $rootScope.$on('$duScrollChanged', gotScroll);
       isObserving = true;
     }
-    spies.push({
-      targetId: targetId,
-      target:   null, 
-      $element: $element, 
-      element:  angular.element($element[0]),
-      offset:   offset
-    });
+    spies.push(spy);
+  }
+
+  function removeSpy(spy) {
+    if(spy === currentlyActive) {
+      currentlyActive = null;
+    }
+    var i = spies.indexOf(spy);
+    if(i !== -1) {
+      spies.splice(i, 1);
+    }
   }
 
   return {
@@ -52,16 +76,12 @@ directive('duScrollspy', function($rootScope, scrollPosition) {
       if (!$attr.href || $attr.href.indexOf('#') === -1) return;
       var targetId = $attr.href.replace(/.*(?=#[^\s]+$)/, '').substring(1);
       if(!targetId) return;
-      addSpy(targetId, $element, -($attr.offset ? parseInt($attr.offset, 10) : 0));
 
-      $scope.$on("$destroy", function() {
-        currentlyActive = null;
-        for (var i = 0; i < spies.length; i++) {
-          if (spies[i].$element === $element) {
-            spies.splice(i, 1);
-            return;
-          }
-        }
+      var spy = new Spy(targetId, $element, -($attr.offset ? parseInt($attr.offset, 10) : 0));
+      addSpy(spy);
+
+      $scope.$on('$destroy', function() {
+        removeSpy(spy);
       });
     }
   };
