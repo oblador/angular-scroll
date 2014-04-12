a09a1a569d56ab88a8fd7c27114dfb59a2f5faba
Joel Arvidsson <joel@oblador.se>
2014-04-12 18:14:42 +0200

    Added angular.element scroll functions.

---
+++ src/helpers.js
@@ -0,0 +1,43 @@
+angular.module('duScroll.scrollHelpers', []).
+run(function($window) {
+  var proto = angular.element.prototype;
+  this.$get = function() {
+    return proto;
+  };
+  
+  var unwrap = function(el) {
+    return el instanceof HTMLElement || el instanceof HTMLDocument ? el : el[0];
+  };
+
+  proto.scrollTo = function(left, top) {
+    var el = unwrap(this);
+    if(el instanceof HTMLDocument) {
+      return $window.scrollTo(left, top);
+    }
+    el.scrollLeft = left;
+    el.scrollTop = top;
+  };
+
+  proto.scrollLeft = function(value) {
+    if(value) {
+      return this.scrollTo(value, this.scrollTop());
+    }
+    var el = unwrap(this);
+    if(el instanceof HTMLDocument) {
+      return $window.scrollX || document.documentElement.scrollLeft || document.body.scrollLeft;
+    }
+    return el.scrollLeft;
+  };
+
+  proto.scrollTop = function(value) {
+    if(value) {
+      return this.scrollTo(this.scrollTop(), value);
+    }
+    var el = unwrap(this);
+    if(el instanceof HTMLDocument) {
+      return $window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+    }
+    return el.scrollTop;
+  };
+
+});
