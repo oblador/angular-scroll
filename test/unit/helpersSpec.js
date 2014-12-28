ee85785b833d1f15077d9b8bd215da10dfd935cc
Joel Arvidsson <joel@oblador.se>
2014-12-28 13:33:19 +0800

    Added unit tests for animation promises.

---
+++ test/unit/helpersSpec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+describe('jqlite helpers', function() {
+  beforeEach(module('duScroll'));
+
+  describe('scrollTopAnimated', function() {
+    var duration = 100;
+
+    it('should return a promise', inject(function($document, $rootScope) {
+      var promise = $document.scrollTopAnimated(100, duration);
+      expect(promise).toEqual(jasmine.any(Object));
+      expect(Object.keys(promise)).toEqual(['then', 'catch', 'finally']);
+
+      it('which should resolve when done animating', function(done){
+        spyOn(promise, 'then');
+        $rootScope.$digest();
+        setTimeout(function() {
+          expect(promise.then).toHaveBeenCalled();
+          done();
+        }, duration*1.5);
+      });
+    }));
+
+    it('should cancel previous animation', function(done){inject(function($document, $rootScope) {
+      var rejected = false;
+      $document.scrollTopAnimated(200, duration)
+      .catch(function() {
+        rejected = true;
+      })
+      .finally(function() {
+        expect(rejected).toEqual(true);
+        done();
+      });
+      $document.scrollTopAnimated(300, duration);
+      $rootScope.$digest();
+    })});
+  });
+});
